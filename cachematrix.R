67c59c449edaac9ad8fa738f334a548ea8a0611c
RakeshNemmani <76504272+RakeshNemmani@users.noreply.github.com>
2020-12-27 17:01:25 -0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,10 +1,19 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This function caches the inverse of the matrix instead of computing repeatedly
 
-## Write a short comment describing this function
+## This function contains an object, that is a matrix, whose inverse is cached
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setmatrix <- function(matrix) m <<- matrix
+  getmatrix <- function() m
+  list(set = set, get = get,
+       setmatrix = setmatrix,
+       getmatrix = getmatrix)
 }
 
 
